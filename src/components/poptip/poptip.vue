51f99ec50ecf24b2f9475ad799abe176e6821684
wangxia <1901399939@qq.com>
2019-12-09 17:04:56 +0800

    add

--- src/components/poptip/poptip.vue
+++ src/components/poptip/poptip.vue
@@ -2,24 +2,38 @@
   <Tooltip ref="tooltip" :theme="theme" :placement="placement" trigger="click">
     <slot></slot>
     <div slot="content" class="blue-poptip">
-      <div class="blue-poptip-content"><i class="yellow-color icon-warn"></i><i class="blue-split"></i>{{content}}</div>
-      <div class="clearfix"><div class="float-right"><button class="blue-btn blue-btn-text blue-btn-xs" @click="close">{{'h.common.cancel' | hlang}}</button><button @click="trigger" class="blue-btn blue-btn-text blue-btn-xs blue-btn-primary">{{'h.common.confirm' | hlang}}</button></div></div>
+      <div class="blue-poptip-content">
+        <i class="yellow-color icon-warn"></i>
+        <i class="blue-split"></i>
+        {{content}}
+      </div>
+      <div class="clearfix">
+        <div class="float-right">
+          <button
+            class="blue-btn blue-btn-text blue-btn-xs"
+            @click="close"
+          >{{'h.common.cancel' | hlang}}</button>
+          <button
+            @click="trigger"
+            class="blue-btn blue-btn-text blue-btn-xs blue-btn-primary"
+          >{{'h.common.confirm' | hlang}}</button>
+        </div>
+      </div>
     </div>
   </Tooltip>
 </template>
 <script>
-
 export default {
-  name: 'bluePoptip',
+  name: "bluePoptip",
   props: {
     content: String,
     placement: {
       type: String,
-      default: 'top'
+      default: "top"
     },
     theme: {
       type: String,
-      default: 'white'
+      default: "white"
     }
   },
   methods: {
@@ -27,7 +41,7 @@ export default {
       this.$refs.tooltip.hide();
     },
     trigger() {
-      this.$emit('confirm');
+      this.$emit("confirm");
       this.close();
     }
   }
